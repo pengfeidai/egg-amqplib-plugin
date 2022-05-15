16257e30d8e2a1570f473d42484d9a3417cc4062
daipengfei <302804389@qq.com>
2022-05-15 15:43:30 +0800

    config: 修改配置文件

--- config/config.default.js
+++ config/config.default.js
@@ -6,24 +6,24 @@
  * @property {String} SOME_KEY - some description
  */
 exports.amqplibPlugin = {
-  app: true,
-  agent: false,
-  // url: 'amqp:localhost:5672'
-  options: {
-    protocol: 'amqp',
-    hostname: 'localhost',
-    port: 5672,
-    username: 'guest',
-    password: 'guest',
-    locale: 'en_US',
-    frameMax: 0,
-    heartbeat: 0,
-    vhost: '/',
+  client: {
+    // url: 'amqp:localhost:5672'
+    options: {
+      protocol: 'amqp',
+      hostname: 'localhost',
+      port: 5672,
+      username: 'guest',
+      password: 'guest',
+      locale: 'en_US',
+      frameMax: 0,
+      heartbeat: 0,
+      vhost: '/',
+    },
+    // socketOptions: {
+    //   cert: certificateAsBuffer, // client cert
+    //   key: privateKeyAsBuffer, // client key
+    //   passphrase: 'MySecretPassword', // passphrase for key
+    //   ca: [caCertAsBuffer], // array of trusted CA certs
+    // },
   },
-  // socketOptions: {
-  //   cert: certificateAsBuffer, // client cert
-  //   key: privateKeyAsBuffer, // client key
-  //   passphrase: 'MySecretPassword', // passphrase for key
-  //   ca: [caCertAsBuffer], // array of trusted CA certs
-  // },
 };
